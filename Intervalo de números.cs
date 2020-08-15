94a0ec77301cba91af445da9f34e457efc478d52
Guilherme da Silva Pereira <62338895+GuiSilva98@users.noreply.github.com>
2020-08-15 19:34:30 -0300

    Add files via upload

---
+++ Intervalo de números.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+
+            System.Console.WriteLine("Insira um número: ");
+            numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (numero >= 0.0 && numero <= 25.0){
+                System.Console.WriteLine("O número "+ numero +" fica no intervalo entre 0 e 25");
+            }
+            else if (numero > 25.0 && numero <= 50.0){
+                System.Console.WriteLine("O número "+ numero +" fica no intervalo entre 25 e 50");
+            }
+            else if (numero > 50.0 && numero <= 75.0){
+                System.Console.WriteLine("O número "+ numero +" fica no intervalo entre 50 e 75");
+            }
+            else if (numero > 75.0 && numero <= 100.0){
+                System.Console.WriteLine("O número "+ numero +" fica no intervalo entre 75 e 100");
+            }
+            else {
+                System.Console.WriteLine("Fora do Intervalo!");
+            }
+        }
+    }
+}
---
+++ Lanches.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo, quantidade;
+            double valor, valorFinal;
+
+            System.Console.WriteLine("Digite o código do item: ");
+            codigo = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Digite a quantidade desejada: ");
+            quantidade = int.Parse(Console.ReadLine());
+
+            if (codigo == 1){
+                System.Console.WriteLine("Você escolheu " + quantidade + " Cahorro(s) Quente(s)");
+                valor = 4;
+            }
+            else if (codigo == 2){
+                System.Console.WriteLine("Você escolheu " + quantidade + " X-Salada(s)");
+                valor = 4.5;
+            }
+            else if (codigo == 3){
+                System.Console.WriteLine("Você escolheu " + quantidade + " X-Bacon(s)");
+                valor = 5;
+            }
+            else if (codigo == 4){
+                System.Console.WriteLine("Você escolheu " + quantidade + " Torrada(s) Simple(s)");
+                valor = 2; 
+            }
+            else if (codigo == 5){
+                System.Console.WriteLine("Você escolheu " + quantidade + " Refrigerante(s)");
+                valor = 1.5;
+            }
+            else{
+                System.Console.WriteLine("Opção não encontrada!");
+                return;
+            }
+            valorFinal = valor * quantidade;
+
+            System.Console.WriteLine("O total a ser pago é de R$" + valorFinal);
+        }
+    }
+}
